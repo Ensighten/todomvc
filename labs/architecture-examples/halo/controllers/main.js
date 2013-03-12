b81673fbc395b380b4e9eb9f96306d65e85df4d7
Todd Wolfson <todd@twolfson.com>
2013-03-11 23:24:39 -0700

    Successfully getting back content =D

--- labs/architecture-examples/halo/controllers/main.js
+++ labs/architecture-examples/halo/controllers/main.js
@@ -1,15 +1,14 @@
 /*global define*/
 'use strict';
 
-console.log('oh hey');
 define(['Builder', 'HtmlController'], function (Builder, HtmlController) {
-    var $ = Builder.$,
-        params = {
-        'start': function (cb) {
-            console.log('oh hey');
-            cb($('<div>Hello world!</div>'));
-        }
-    };
+    var params = {
+            'name': 'main',
+            'start': function (cb) {
+                console.log('oh hey');
+                cb(Builder('<div>Hello world!</div>'));
+            }
+        };
 
     return HtmlController(params);
 });
