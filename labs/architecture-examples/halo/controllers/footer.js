f44c74ff08a907a35bc786e5ce3bd450606465bf
Todd Wolfson <todd@twolfson.com>
2013-03-16 21:02:48 -0700

    All done with app -- now to do JSHint cleanup in a separate branch and whatnot

--- labs/architecture-examples/halo/controllers/footer.js
+++ labs/architecture-examples/halo/controllers/footer.js
@@ -3,20 +3,30 @@ define(['Sauron', 'Builder', 'mvc!v/footer', 'HtmlController', 'mvc!m/todos', 'm
         'name': 'footer',
         'start': function (todos, cb) {
           // Render our content
-          var completed = todos.filter(function (todo) {
+          var completedTodos = todos.filter(function (todo) {
                 return todo.completed;
-              }).length,
+              }),
+              completed = completedTodos.length,
               data = {
                 remaining: todos.length - completed,
                 completed: completed
               },
-              $html = Builder(tmpl, data);
+              $html = Builder(tmpl, data),
+              $clearCompleted = $html.filter('#clear-completed');
 
           // When any of the footer links are clicked, update the state
           $html.on('radio-select', '.radio-item', function () {
             var $a = $(this),
                 filter = $a.data('filter');
             Sauron.model('state').update({filter: filter});
+          });
+
+          // When the clear completed button is clicked
+          $clearCompleted.on('click', function () {
+            // Delete each of the completed todos
+            completedTodos.forEach(function (todo) {
+              Sauron.model('todos')['delete'](todo);
+            });
           });
 
           // Callback with content
