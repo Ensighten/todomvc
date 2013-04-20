2c9b05f04ce92a6f495c8aa50719fe230a94ea37
Todd Wolfson <todd@twolfson.com>
2013-04-19 21:01:21 -0700

    Fixed rebase command in rm.dev.sh

--- rm.dev.sh
+++ rm.dev.sh
@@ -1,3 +1,7 @@
+# Push out latest changes
+git push origin
+git push twolfson
+
 # Move to export branch
 git branch -D dev/implement.halo.squashed
 git checkout -b dev/implement.halo.squashed
@@ -10,7 +14,7 @@ git add -A
 git commit -m "Removed dev items"
 
 # Squash commits
-git rebase master -i
+git rebase gh-pages -i
 
 # Push changes to remote
 git push origin dev/implement.halo.squashed --force
